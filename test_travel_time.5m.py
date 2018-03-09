2c9ed45e951a2b040639564666b65521aeca34a6
Daniel Parker <danpker@gmail.com>
2018-03-09 09:30:40 +0000

    Clean up and fixes
    - linting and styling fixes
    - adding more tests
    - add requirements file
    - fixes #2

--- test_travel_time.5m.py
+++ test_travel_time.5m.py
@@ -5,29 +5,30 @@ import random
 import imp
 travel_time = imp.load_source('travel_time.5m', 'travel_time.5m.py')
 
+
 class GetColourTestCase(unittest.TestCase):
 
     def test_less_than_ten_minutes_is_green(self):
         """A duration of less than 10 minutes will have a colour of green."""
         duration = random.randint(1, 50)
-        diff = random.randint(0,9)
+        diff = random.randint(0, 9)
         duration_in_traffic = duration + diff
 
         colour = travel_time.get_colour(f"{duration} mins",
                                         f"{duration_in_traffic} mins")
 
-        self.assertEqual(colour, "green")
+        self.assertEqual("green", colour)
 
     def test_between_ten_mins_and_twenty_five_is_orange(self):
         """A duration of between 10 mins and 25 is orange."""
         duration = random.randint(1, 35)
-        diff = random.randint(9, 25)
+        diff = random.randint(10, 24)
         duration_in_traffic = duration + diff
 
         colour = travel_time.get_colour(f"{duration} mins",
                                         f"{duration_in_traffic} mins")
 
-        self.assertEqual(colour, "orange")
+        self.assertEqual("orange", colour)
 
     def test_over_twenty_five_mins_is_red(self):
         """A duration over 25 minutes is red."""
@@ -38,7 +39,58 @@ class GetColourTestCase(unittest.TestCase):
         colour = travel_time.get_colour(f"{duration} mins",
                                         f"{duration_in_traffic} mins")
 
-        self.assertEqual(colour, "red")
+        self.assertEqual("red", colour)
+
+    def test_will_handle_durations_in_hours(self):
+        """If the duration will take over an hour it will be in the format:
+        "2 hours 48 mins". durations like this should be handled correctly"""
+        duration = random.randint(1, 50)
+        diff = random.randint(0, 9)
+        duration_in_traffic = duration + diff
+
+        colour = travel_time.get_colour(f"1 hour {duration} mins",
+                                        f"1 hour {duration_in_traffic} mins")
+
+        self.assertEqual("green", colour)
+
+    def test_will_be_orange_even_with_hours(self):
+        """If the duration will take over an hour it will be in the format:
+        "2 hours 48 mins". durations like this should be handled correctly.
+        A duration of between 10 mins and 25 is orange."""
+        duration = random.randint(1, 35)
+        diff = random.randint(10, 25)
+        duration_in_traffic = duration + diff
+
+        colour = travel_time.get_colour(f"1 hour {duration} mins",
+                                        f"1 hour {duration_in_traffic} mins")
+
+        self.assertEqual("orange", colour)
+
+    def test_will_be_red_even_with_hours(self):
+        """If the duration will take over an hour it will be in the format:
+        "2 hours 48 mins". durations like this should be handled correctly.
+        A duration over 25 minutes is red."""
+        duration = 1
+        diff = random.randint(25, 59)
+        duration_in_traffic = duration + diff
+
+        colour = travel_time.get_colour(f"1 hour {duration} mins",
+                                        f"1 hour {duration_in_traffic} mins")
+
+        self.assertEqual("red", colour)
+
+    def test_negative_difference_should_be_green(self):
+        """If we are lucky enough for the expected time to be negative, the
+        colour should be green."""
+        duration = 60
+        diff = random.randint(1, 59)
+        duration_in_traffic = duration - diff
+
+        colour = travel_time.get_colour(f"{duration} mins",
+                                        f"{duration_in_traffic} mins")
+
+        self.assertEqual("green", colour)
+
 
 if __name__ == "__main__":
     unittest.main()
--- travel_time.5m.py
+++ travel_time.5m.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env LC_ALL=en_US.UTF-8 /usr/local/bin/python3
 
 import requests
+import dateparser
+from datetime import datetime
 import json
 import re
 import html
 import base64
 import os
 
-START = ''
-DESTINATION = ''
-API_KEY = ''
-TAG_RE = re.compile(r'<[^>]+>')
+START = ""
+DESTINATION = ""
+API_KEY = ""
+TAG_RE = re.compile(r"<[^>]+>")
 
 
 def main():
-    url = ('https://maps.googleapis.com/maps/api/directions/json?'
-           'origin={}&destination={}&mode=driving&departure_time=now&'
-           'key={}'.format(START, DESTINATION, API_KEY))
+    url = ("https://maps.googleapis.com/maps/api/directions/json?"
+           "origin={}&destination={}&mode=driving&departure_time=now&"
+           "key={}".format(START, DESTINATION, API_KEY))
     data = json.loads(requests.get(url).content)
-    routes = data.get('routes')
+    routes = data.get("routes")
     parse_route(routes[0])
 
 
@@ -28,61 +30,78 @@ def load_config():
     global API_KEY
 
     directory = os.path.dirname(os.path.realpath(__file__))
-    with open(os.path.join(directory, 'config.json')) as f:
+    with open(os.path.join(directory, "config.json")) as f:
         json_config = json.loads(f.read())
 
-    START = json_config.get('start')
-    DESTINATION = json_config.get('destination')
-    API_KEY = json_config.get('api_key')
+    START = json_config.get("start")
+    DESTINATION = json_config.get("destination")
+    API_KEY = json_config.get("api_key")
 
 
 def parse_route(route):
-    leg = route.get('legs')[0]
-    duration = leg.get('duration').get('text')
-    duration_in_traffic = leg.get('duration_in_traffic').get('text')
-    print('{}|color={}'.format(
+    leg = route.get("legs")[0]
+    duration = leg.get("duration").get("text")
+    duration_in_traffic = leg.get("duration_in_traffic").get("text")
+    print("{}|color={}".format(
         duration_in_traffic, get_colour(duration, duration_in_traffic)))
-    print('---')
+    print("---")
     print(get_steps(leg).encode().decode())
-    print('{} | image={}'.format(route.get('summary'), get_map(route)))
+    print("{} | image={}".format(route.get("summary"), get_map(route)))
 
 
 def get_colour(duration, duration_in_traffic):
-    first = int(re.findall('\d+', duration)[0])
-    second = int(re.findall('\d+', duration_in_traffic)[0])
+    """Return a colour to provide an idea of the current traffic.
 
-    difference = second - first
+    Gives a visual representation of the amount of traffic on route.
 
-    if difference < 10:
-        return 'green'
-    elif difference >= 10 and difference <= 25:
-        return 'orange'
+    :param duration: A duration string, like "4 mins"
+    :param duration_in_traffic: A duration in traffic
+    """
+    now = datetime.now()
+    # dateparser will put the duration in traffic before the duration without
+    # traffic. In this funtion, we basically use it to work out the potential
+    # set off time for each scenario with the same arrival time
+    set_off_time = dateparser.parse(duration,  settings={"RELATIVE_BASE": now})
+    set_off_time_in_traffic = dateparser.parse(
+        duration_in_traffic,  settings={"RELATIVE_BASE": now})
+
+    if set_off_time_in_traffic > set_off_time:
+        # If real duration is less than the normal duration, return green
+        return "green"
+
+    difference = set_off_time - set_off_time_in_traffic
+    minutes = difference.seconds / 60
+
+    if minutes < 10:
+        return "green"
+    elif minutes >= 10 and minutes <= 25:
+        return "orange"
     else:
-        return 'red'
+        return "red"
 
 
 def get_steps(leg):
-    return '\n'.join([step_to_string(step) for step in leg.get('steps')])
+    return "\n".join([step_to_string(step) for step in leg.get("steps")])
 
 
 def step_to_string(step):
-    return '{} - {}'.format(clean(step.get('html_instructions')),
-                            step.get('duration').get('text'))
+    return "{} - {}".format(clean(step.get("html_instructions")),
+                            step.get("duration").get("text"))
 
 
 def get_map(route):
-    points = route.get('overview_polyline').get('points')
-    url = ('https://maps.googleapis.com/maps/api/staticmap?'
-           'size=640x400&path=enc%3A{}&key={}'.format(points, API_KEY))
+    points = route.get("overview_polyline").get("points")
+    url = ("https://maps.googleapis.com/maps/api/staticmap?"
+           "size=640x400&path=enc%3A{}&key={}".format(points, API_KEY))
     image = requests.get(url).content
-    return base64.b64encode(image).decode('utf-8')
+    return base64.b64encode(image).decode("utf-8")
 
 
 def clean(text):
     text = html.unescape(text)
-    return TAG_RE.sub('', text)
+    return TAG_RE.sub("", text)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     load_config()
     main()
